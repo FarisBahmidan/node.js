5a2f14a8b2e105be714b349f572bc0ff36ab4c3c
developer-man-ui <tomy.lamablama@gmail.com>
2023-06-15 19:22:39 +0200

    proud v1.0.0

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express, { Response } from "express";
+import https from "https"
 import path from "path";
+import fs from "fs"
 
 const htmlPublic = (res: Response, htmldir: string, file_name: string = "index.html") => {
     return res.sendFile(path.join(__dirname, `public/${htmldir}/${file_name}`))
 }
-
+const ssl_config = {
+    ssl_files_path:`C:/Certbot/archive/farisab.com`,
+    privte_key_failename:"privkey1.pem",
+    cert_filename:"fullchain1.pem"
+}
 const app = express()
+const cert =  fs.readFileSync(path.join(ssl_config.ssl_files_path, ssl_config.cert_filename))
+const privte_key = fs.readFileSync(path.join(ssl_config.ssl_files_path, ssl_config.privte_key_failename))
+const appSSl = https.createServer({
+    cert:cert,
+    key:privte_key
+}, app)
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
@@ -14,8 +26,9 @@ app.get('/', (req, res) => {
     res.redirect('/index')
 
 })
-
-app.listen(443, () => {
+app.all(/([A-z]|[0-9])/g, (req, res) => {
+    res.send("page not found")
+})
+appSSl.listen(443, () => {
     console.log("servar satrting in port 443");
-
 })
